f1fd16f705cd007a1fd824f9a4a1a8e6dd729208
Joshua Bakasa <baksajoshua09@gmail.com>
2020-04-30 00:31:15 +0300

    Adding the lab id to the payload

--- app/Api/V1/Controllers/ConsumptionsController.php
+++ app/Api/V1/Controllers/ConsumptionsController.php
@@ -165,7 +165,8 @@ class ConsumptionsController extends Controller
 		$data = [];													
 		foreach ($consumptions as $conskey => $consumption) {
 			$data[$conskey] = [
-					'lab' => $consumption->lab->name,
+					'labid' => $consumption->lab->id,
+					'labname' => $consumption->lab->name,
 					'start_of_week' => $consumption->start_of_week,
 					'end_of_week' => $consumption->end_of_week,
 					'week' => $consumption->week
