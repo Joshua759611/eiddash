4fa33f474219c8685330beaf65c423af34d3fd89
Joshua Bakasa <baksajoshua09@gmail.com>
2020-05-26 09:52:26 +0300

    Updated the Covid Kits

--- database/seeds/CovidKitsSeeder.php
+++ database/seeds/CovidKitsSeeder.php
@@ -14,48 +14,34 @@ class CovidKitsSeeder extends Seeder
      */
     public function run()
     {
-    	CovidConsumption::truncate();
-        CovidConsumptionDetail::truncate();
+    	// CovidConsumption::truncate();
+     //    CovidConsumptionDetail::truncate();
         CovidKit::truncate();
         $kits = [
         		['material_no' => '9175431190', 'product_description' => 'Cobas® SARS-CoV-2 Test',
-        		'pack_size' => 192, 'calculated_pack_size' => 192, 'type' => 'Kit'],
-        		['material_no' => '9175440190', 'product_description' => 'Cobas® SARS-CoV-2 Control Kit',
-        		'pack_size' => 16, 'calculated_pack_size' => 1536, 'type' => 'Kit'],
-        		['material_no' => '7002238190', 'product_description' => 'Cobas® Buffer Negative Control Kit',
-        		'pack_size' => 16, 'calculated_pack_size' => 1536, 'type' => 'Kit'],
-        		['material_no' => '5534917001', 'product_description' => 'Cobas OMNI Processing Plate',
-        		'pack_size' => 32, 'calculated_pack_size' => 1536, 'type' => 'Kit'],
-        		['material_no' => '5534925001', 'product_description' => 'Cobas OMNI Pipette Tips',
-        		'pack_size' => 1536, 'calculated_pack_size' => 768, 'type' => 'Kit'],
-        		['material_no' => '5534941001', 'product_description' => 'Cobas OMNI Amplification Plate',
-        		'pack_size' => 32, 'calculated_pack_size' => 3072, 'type' => 'Kit'],
-        		['material_no' => '6997503190', 'product_description' => 'Kit Cobas 6800/8800 Wash Reagent IVD (4.2 L)',
-        		'pack_size' => 288, 'calculated_pack_size' => 288, 'type' => 'Kit'],
-        		['material_no' => '6997511190', 'product_description' => 'Kit Cobas 6800/8800 SPEC DIL REAGENT IVD (4 x 875 mL)',
-        		'pack_size' => 1152, 'calculated_pack_size' => 1152, 'type' => 'Kit'],
-        		['material_no' => '6997538190', 'product_description' => 'Kit Cobas 6800/8800 LYS REAGENT IVD (4 x 875 mL)',
-        		'pack_size' => 1152, 'calculated_pack_size' => 1152, 'type' => 'Kit'],
-        		['material_no' => '6997546190', 'product_description' => 'Kit Cobas 6800/8800 MGP IVD',
-        		'pack_size' => 480, 'calculated_pack_size' => 480, 'type' => 'Kit'],
-        		['material_no' => '8030073001', 'product_description' => 'Solid Waste Bag Set of 20',
-        		'pack_size' => 20, 'calculated_pack_size' => 7680, 'type' => 'Kit'],
-        		['material_no' => '6438776001', 'product_description' => 'Cobas omni Secondary Tubes 13x75 (optional)*',
-        		'pack_size' => 1500, 'calculated_pack_size' => 1500, 'type' => 'Kit'],
-                ['material_no' => 'manual-1', 'product_description' => 'SARS-COV-2 Extraction Kits',
-                'pack_size' => 240, 'calculated_pack_size' => 240, 'type' => 'Manual', 'unit' => 'tests'],
-                ['material_no' => 'manual-2', 'product_description' => 'SARS-Cov2 Primers and probes- 96 tests',
-                'pack_size' => 96, 'calculated_pack_size' => 96, 'type' => 'Manual', 'unit' => 'tests'],
-                ['material_no' => 'manual-3', 'product_description' => 'AgPath as a kit (Enzyme and buffer)',
-                'pack_size' => 1000, 'calculated_pack_size' => 1000, 'type' => 'Manual', 'unit' => 'tests'],
-                ['material_no' => 'manual-4', 'product_description' => '10µl Sterile Filtered Pipette tips ',
-                'pack_size' => 960, 'calculated_pack_size' => 960, 'type' => 'Manual', 'unit' => 'tips'],
-                ['material_no' => 'manual-5', 'product_description' => '100µl Sterile Filtered Pipette tips',
-                'pack_size' => 960, 'calculated_pack_size' => 960, 'type' => 'Manual', 'unit' => 'tips'],
-                ['material_no' => 'manual-6', 'product_description' => '200µl Sterile Filtered Pipette tips',
-                'pack_size' => 960, 'calculated_pack_size' => 960, 'type' => 'Manual', 'unit' => 'tips'],
-                ['material_no' => 'manual-7', 'product_description' => '1000µl Sterile Filtered Pipette tips',
-                'pack_size' => 960, 'calculated_pack_size' => 960, 'type' => 'Manual', 'unit' => 'tips'],
+                'pack_size' => 192, 'calculated_pack_size' => 192, 'type' => 'Kit'],
+                ['material_no' => '9175440190', 'product_description' => 'Cobas® SARS-CoV-2 Control Kit',
+                'pack_size' => 16, 'calculated_pack_size' => 1536, 'type' => 'Kit'],
+                ['material_no' => '7002238190', 'product_description' => 'Cobas® Buffer Negative Control Kit',
+                'pack_size' => 16, 'calculated_pack_size' => 1536, 'type' => 'Kit'],
+                ['material_no' => '5534917001', 'product_description' => 'Cobas OMNI Processing Plate',
+                'pack_size' => 32, 'calculated_pack_size' => 1536, 'type' => 'Kit'],
+                ['material_no' => '5534925001', 'product_description' => 'Cobas OMNI Pipette Tips',
+                'pack_size' => 1536, 'calculated_pack_size' => 768, 'type' => 'Kit'],
+                ['material_no' => '5534941001', 'product_description' => 'Cobas OMNI Amplification Plate',
+                'pack_size' => 32, 'calculated_pack_size' => 3072, 'type' => 'Kit'],
+                ['material_no' => '6997503190', 'product_description' => 'Kit Cobas 6800/8800 Wash Reagent IVD (4.2 L)',
+                'pack_size' => 288, 'calculated_pack_size' => 288, 'type' => 'Kit'],
+                ['material_no' => '6997511190', 'product_description' => 'Kit Cobas 6800/8800 SPEC DIL REAGENT IVD (4 x 875 mL)',
+                'pack_size' => 1152, 'calculated_pack_size' => 1152, 'type' => 'Kit'],
+                ['material_no' => '6997538190', 'product_description' => 'Kit Cobas 6800/8800 LYS REAGENT IVD (4 x 875 mL)',
+                'pack_size' => 1152, 'calculated_pack_size' => 1152, 'type' => 'Kit'],
+                ['material_no' => '6997546190', 'product_description' => 'Kit Cobas 6800/8800 MGP IVD',
+                'pack_size' => 480, 'calculated_pack_size' => 480, 'type' => 'Kit'],
+                ['material_no' => '8030073001', 'product_description' => 'Solid Waste Bag Set of 20',
+                'pack_size' => 20, 'calculated_pack_size' => 7680, 'type' => 'Kit'],
+                ['material_no' => '6438776001', 'product_description' => 'Cobas omni Secondary Tubes 13x75 (optional)*',
+                'pack_size' => 1500, 'calculated_pack_size' => 1500, 'type' => 'Kit'],
                 ['material_no' => 'P1', 'product_description' =>  'Oral pharyngeal swabs (Without media)', 'unit'=> 'pack', 'pack_size' => 25, 'type' => 'Consumable'],
                 ['material_no' => 'P2', 'product_description' =>  'Nasopharyngeal swabs (Without media)', 'unit'=> 'pack', 'pack_size' => 1000, 'type' => 'Consumable'],
                 ['material_no' => 'P3', 'product_description' =>  'Ziplock bags small 6*9inch', 'unit'=> 'pack', 'pack_size' => 1000, 'type' => 'Consumable'],
@@ -81,7 +67,6 @@ class CovidKitsSeeder extends Seeder
                 ['material_no' => 'P23', 'product_description' => 'DNA Away', 'unit'=> 'ml', 'pack_size' => 250, 'type' => 'Consumable'],
                 ['material_no' => 'P24', 'product_description' => 'Eppendorf tubes', 'unit'=> 'Boxes', 'pack_size' => 500, 'type' => 'Consumable'],
                 ['material_no' => 'P25', 'product_description' => 'Chlorine, HTH 70%', 'unit'=> 'Litres', 'pack_size' => 5, 'type' => 'Consumable'],
-                ['material_no' => 'P26', 'product_description' => 'Absolute ethanol', 'unit'=> 'Liters', 'pack_size' => 5, 'type' => 'Consumable'],
                 ['material_no' => 'P27', 'product_description' => 'Alcohol-based sanitizer', 'unit'=> 'Boxes', 'pack_size' => 30, 'type' => 'Consumable'],
                 ['material_no' => 'P28', 'product_description' => 'Theatre gown, protective,disposable  (blue)', 'unit'=> 'pack', 'pack_size' => 20, 'type' => 'Consumable'],
                 ['material_no' => 'P29', 'product_description' => 'plastic apron, disposable', 'unit'=> 'pack', 'pack_size' => 250, 'type' => 'Consumable'],
@@ -92,7 +77,19 @@ class CovidKitsSeeder extends Seeder
                 ['material_no' => 'P34', 'product_description' => 'Shoe covers(disposable)', 'unit'=> 'pack', 'pack_size' => 10, 'type' => 'Consumable'],
                 ['material_no' => 'P35', 'product_description' => 'Head covers(disposable)', 'unit'=> 'pack', 'pack_size' => 100, 'type' => 'Consumable'],
                 ['material_no' => 'P36', 'product_description' => 'Paper towels', 'type' => 'Consumable'],
-                ['material_no' => 'P37', 'product_description' => 'Tongue depressors', 'type' => 'Consumable']
+                ['material_no' => 'P37', 'product_description' => 'Tongue depressors', 'type' => 'Consumable'],
+                ['material_no' => 'M1', 'product_description' => 'SARS-COV-2 Extraction Kits',
+                'pack_size' => 240, 'calculated_pack_size' => 240, 'type' => 'Manual', 'unit' => 'tests'],
+                ['material_no' => 'M2', 'product_description' => 'SARS-Cov2 Primers and probes- 96 tests',
+                'pack_size' => 96, 'calculated_pack_size' => 96, 'type' => 'Manual', 'unit' => 'tests'],
+                ['material_no' => 'M3', 'product_description' => '10µl Sterile Filtered Pipette tips ',
+                'pack_size' => 960, 'calculated_pack_size' => 960, 'type' => 'Manual', 'unit' => 'tips'],
+                ['material_no' => 'M4', 'product_description' => '100µl Sterile Filtered Pipette tips',
+                'pack_size' => 960, 'calculated_pack_size' => 960, 'type' => 'Manual', 'unit' => 'tips'],
+                ['material_no' => 'M5', 'product_description' => '200µl Sterile Filtered Pipette tips',
+                'pack_size' => 960, 'calculated_pack_size' => 960, 'type' => 'Manual', 'unit' => 'tips'],
+                ['material_no' => 'M6', 'product_description' => '1000µl Sterile Filtered Pipette tips',
+                'pack_size' => 960, 'calculated_pack_size' => 960, 'type' => 'Manual', 'unit' => 'tips'],
         	];
         foreach ($kits as $key => $kit)
         	CovidKit::create($kit);
@@ -100,11 +97,3 @@ class CovidKitsSeeder extends Seeder
         // return true;
     }
 }
-
-
-
-
-
-
-
-
