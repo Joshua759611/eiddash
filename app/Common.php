2d7a04c15ecb76b36c567434480c12437523c4d2
Joel <joelkith@gmail.com>
2020-09-28 12:54:41 +0300

    Updates

--- app/Common.php
+++ app/Common.php
@@ -21,6 +21,40 @@ class Common
     {
         Mail::to(['joelkith@gmail.com'])->send(new CustomMail());
     }
+    
+	public static function csv_download($data, $file_name='page-data-export', $first_row=true, $save_file=false)
+	{
+		if(!$data) return;
+
+		if($save_file){
+			$fp = fopen(storage_path("exports/{$file_name}.csv"), 'w');
+		}else{
+			header('Content-Description: File Transfer');
+			header('Content-Type: application/csv');
+			header("Content-Disposition: attachment; filename={$file_name}.csv");
+			// header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+			
+			$fp = fopen('php://output', 'w');			
+		}
+		// ob_clean();
+
+		if($first_row){
+
+			$first = [];
+
+			foreach ($data[0] as $key => $value) {
+				$first[] = $key;
+			}
+			fputcsv($fp, $first);
+
+		}
+
+		foreach ($data as $key => $value) {
+			fputcsv($fp, $value);
+		}
+		// ob_flush();
+		fclose($fp);
+	}
 
 
 	public static function get_days($start, $finish, $with_holidays=true)
