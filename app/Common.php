f0679fa8d847e1c3be2566d73f4b4bb4614e91ad
Joshua Bakasa <baksajoshua09@gmail.com>
2020-10-12 10:49:12 +0300

    Get the response code

--- app/Common.php
+++ app/Common.php
@@ -334,7 +334,8 @@ class Common
 			],
 		]);
 		$body = json_decode($response->getBody());
-		print_r($body->responses[0]);
+		print_r($response->getStatusCode());
+		// print_r($body->responses[0]);
 
 		if($response->getStatusCode() > 399) {
         	return false;
