83360dc7e118939b3840932bdff31dd2ad982314
Joshua Bakasa <baksajoshua09@gmail.com>
2020-10-26 14:42:18 +0300

    Bug fix

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -1192,7 +1192,6 @@ class ReportController extends Controller
             $model = $model->where('view_facilitys.subcounty_id', '=', auth()->user()->level);
         if (auth()->user()->user_type_id == 8){
             $user_level = auth()->user()->facility_id;
-            dd($user_level);
             $model = $model->whereRaw("($table.facility_id = {$user_level} OR $table.lab_id = {$user_level} OR $table.user_id = {$user_level})");
         }
 
