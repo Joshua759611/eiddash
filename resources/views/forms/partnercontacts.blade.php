fc1e5795eeec126c93b2ee5b5676dc9b2aa1e87a
agesa3 <collinsagesa2000@gmail.com>
2022-03-06 16:40:35 +0300

    corrected the functionality of the critical result checkbox when updated

--- resources/views/forms/partnercontacts.blade.php
+++ resources/views/forms/partnercontacts.blade.php
@@ -56,11 +56,8 @@
                         <label class="col-sm-4 control-label">Notifaction Type</label>
                         <div class="col-sm-8">
                             <label class="control-label">Critical Results</label>
-                            <input type="checkbox" class="i-checks" name="critical_results" value="1" 
-                                @if(isset($contact) && $contact->critical_results)
-                                    checked
-                                @endif
-                             />
+                            <input  type="hidden" name="critical_results" value="0"/>
+                            <input type="checkbox" name="critical_results" value="1" {{ old('critical_results', isset($contact) ? 'checked' : '') }}/>
                         </div>
                     </div>
                     <div class="form-group">
