68c3843382a24585363917bd4d824017c9fbda1f
rosaga <richardjohnzranger@gmail.com>
2022-04-29 15:52:31 +0300

    vl summary files

---
+++ resources/views/mail/summary.blade.php
@@ -0,0 +1,25 @@
+<p>
+	{{ $name }},
+	<br />
+
+
+		Please find attached summary of HIV-Viral load status report between {{ $range ?? '' }}.
+
+		<br />
+
+		Thanks.
+
+		<br />
+
+		--
+
+		<br />
+
+		VL Support Team
+
+		<br />
+
+		This email was automatically generated. Please do not respond to this email address or it will be ignored.
+
+	
+</p>
