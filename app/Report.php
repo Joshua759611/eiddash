d8fcb13eb68f4c124d5b516ace95628dbb565400
injiri <simoninjiri@gmail.com>
2022-09-09 11:22:27 +0300

    sent criticals for -7days from now()

--- app/Report.php
+++ app/Report.php
@@ -436,7 +436,7 @@ class Report
     {
         $sampleview_class = self::$my_classes[$type]['sampleview_class'];
         $view_table = self::$my_classes[$type]['view_table'];
-        $dt = date('Y-m-d', strtotime('-1 days'));
+        $dt = date('Y-m-d', strtotime('-7 days'));
         $q = 'rcategory IN (3, 4)';
         $lab = \App\Lab::find(env('APP_LAB'));
         if ($type == 'eid') $q = 'result=2';
